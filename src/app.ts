55492bdb2c258eaa5feb5710aa8270f28f0242cb
msfukui <msfukui@gmail.com>
2024-12-01 22:22:55 +0900

    feat: Impl Task Counter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 import './assets/style.css';
 
-async function addSticky() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
+async function taskCountsForSelectedCards() {
+  miro.board.ui.on('selection:update', async (event) => {
+    let selectedItems = event.items;
+    let cards = selectedItems.filter(item => item.type === 'card');
 
-  await miro.board.viewport.zoomTo(stickyNote);
+    if (cards.length === 0) {
+      miro.board.notifications.showInfo('(unselected)');
+      return;
+    }
+
+    let uncheckedTasksCount = 0;
+    let checkedTasksCount = 0;
+    cards.forEach((card) => {
+      const uncheckedTasks = card.description.match(/\<li data\-list\=\"unchecked\"/g) ?? [];
+      uncheckedTasksCount += uncheckedTasks.length;
+      const checkedTasks = card.description.match(/\<li data\-list\=\"checked\"/g) ?? [];
+      checkedTasksCount += checkedTasks.length;
+    });
+    miro.board.notifications.showInfo(`Task: ${checkedTasksCount}/${uncheckedTasksCount + checkedTasksCount}`);
+  });
 }
 
-addSticky();
+taskCountsForSelectedCards();
